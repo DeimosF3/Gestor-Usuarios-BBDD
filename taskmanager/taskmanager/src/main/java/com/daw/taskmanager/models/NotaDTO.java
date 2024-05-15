8d50b4c1fcd24b67c0d64402cbdeb8518f51ae16
Rodrigo Ramírez López <cientoundalmatas693@gmail.com>
2024-05-15 14:00:38 +0200

    Create NotaDTO.java

---
+++ taskmanager/taskmanager/src/main/java/com/daw/taskmanager/models/NotaDTO.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.daw.taskmanager.models;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+/**
+ *
+ * @author Rodrigo RL
+ */
+public class NotaDTO {
+    @NotEmpty(message = "Nombre obligatorio")
+    private String nombre;
+    @Size(min = 10, message = "La descripción debe tener al menos 10 caracteres")
+    @Size(max = 2000, message = "La descripción no puede exceder de 2000 caracteres")
+    private String descripcion;
+    
+}
